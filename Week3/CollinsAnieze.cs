b7ab6950153a66848d18efe668f677ecbf84fea2
collins Anieze <chigoziecollins434@gmail.com>
2021-10-01 22:29:10 +0100

    My solution.

---
+++ Week3/CollinsAnieze.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public static string TwoString(string str1, string str2)
+        {
+
+            public static string TwoString(string str1, string str2)
+            {
+
+                for (int i = 0; i < str1.Length && i < str2.Length; i++)
+                {
+                    if (str1[i] == str2[i])
+                    {
+                        return "YES";
+                    }
+                }
+
+                return "NO";
+            }
+        }
+    }
+}
