c191c28c957b314894ee8df9058b4180b6dc9f25
Chigozie Norbert Madubuko <Chigoziemadubuko@gmail.com>
2021-10-02 18:26:39 +0100

    Two string solution with Arrays

---
+++ Week3/chigozie_twoStrings_with_array.js
@@ -0,0 +1,12 @@
+function twoStrings(str1 = "and", str2 = "art") {
+  const arr = new Array(26).fill(false);
+  for (let i = 0; i < str1.length; i++) {
+    arr[str1[i].charCodeAt() - "a".charCodeAt()] = true;
+  }
+  for (let j = 0; j < str2.length; j++) {
+    if (arr[str2[j].charCodeAt() - "a".charCodeAt()]) {
+      return "YES";
+    }
+  }
+  return "NO";
+}
